7f0c9be60414646a5fb20ee1b4e4794cdc2a15d9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-25 22:16:52 -0300

    feat: findbyId, insert, update, delete service e controller

--- src/main/java/com/github/cidarosa/ms/produto/service/ProdutoService.java
+++ src/main/java/com/github/cidarosa/ms/produto/service/ProdutoService.java
@@ -3,6 +3,7 @@ package com.github.cidarosa.ms.produto.service;
 import com.github.cidarosa.ms.produto.dto.ProdutoDTO;
 import com.github.cidarosa.ms.produto.entities.Produto;
 import com.github.cidarosa.ms.produto.repositories.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,5 +22,53 @@ public class ProdutoService {
         List<Produto> produtos = produtoRepository.findAll();
 
         return produtos.stream().map(ProdutoDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ProdutoDTO findProdutoById(Long id){
+
+        Produto produto = produtoRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Recurso não encontrado. ID: " + id)
+        );
+
+        return new ProdutoDTO(produto);
+    }
+
+    @Transactional
+    public ProdutoDTO saveProduto(ProdutoDTO produtoDTO){
+
+        Produto produto = new Produto();
+        copyDtoToProduto(produtoDTO, produto);
+        produto = produtoRepository.save(produto);
+        return new ProdutoDTO(produto);
+    }
+
+    @Transactional
+    public ProdutoDTO updateProduto(Long id, ProdutoDTO produtoDTO){
+
+        try {
+            Produto produto = produtoRepository.getReferenceById(id);
+            copyDtoToProduto(produtoDTO, produto);
+            return new ProdutoDTO(produto);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Recurso não encontrado. ID: " + id);
+        }
+    }
+
+    @Transactional
+    public void deleteProdutoById(Long id){
+
+        if(!produtoRepository.existsById(id)){
+            throw new EntityNotFoundException("Recurso não encontrado. ID: " + id);
+        }
+
+        produtoRepository.deleteById(id);
+    }
+
+    private void copyDtoToProduto(ProdutoDTO produtoDTO, Produto produto) {
+
+        produto.setNome(produto.getNome());
+        produto.setDescricao(produtoDTO.getDescricao());
+        produto.setValor(produtoDTO.getValor());
     }
 }
