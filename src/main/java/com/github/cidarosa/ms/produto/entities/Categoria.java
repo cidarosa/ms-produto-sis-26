395345de7bbf5330871f5d11cdc5215a240ebed9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-11 19:15:01 -0300

    feat: organizando imports

--- src/main/java/com/github/cidarosa/ms/produto/entities/Categoria.java
+++ src/main/java/com/github/cidarosa/ms/produto/entities/Categoria.java
@@ -1,7 +1,16 @@
 package com.github.cidarosa.ms.produto.entities;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
